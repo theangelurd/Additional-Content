e6de4b30730cd2c3ba1535601190f40b17ec8eb4
theangelurd <71867069+theangelurd@users.noreply.github.com>
2020-12-28 22:40:30 -0400

    Add files via upload

---
+++ scripts/emra.js
@@ -0,0 +1,16 @@
+const templura = new Planet("emra", Planets.sun, 1.2, 0.9);
+emra.generator = new SerpuloPlanetGenerator();
+emra.mesh = new HexMesh(emra, 6);
+emra.bloom = true;
+emra.radius = 2;
+emra.accessible = true;
+emra.hasAtmosphere = true;
+emra.atmosphereColor = Color.valueOf("c7fffbff");
+emra.atmosphereRadIn = 0.04;
+emra.atmosphereRadOut = 0.3;
+emra.localizedName = "Emra";
+
+const navalValley = new SectorPreset("navalValley", emra, 18);
+navalValley.captureWave = 25;
+navalValley.localizedName = "Naval Valley";
+navalValley.difficulty = 4;
