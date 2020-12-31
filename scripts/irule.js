22d26fa271c51bc6d55e4541562c7ff08cd7dc18
theangelurd <71867069+theangelurd@users.noreply.github.com>
2020-12-30 20:53:02 -0400

    Delete irule.js

--- scripts/irule.js
+++
@@ -1,5 +0,0 @@
-const irule = extendContent(mech, "irule", {});
-irule.constructor = function(){
-  return extend(PayloadUnit, {});
-};
-irule.abilities.add(new ForceFieldAbility(16, 8/60, 280, 80 * 5, new ShieldRegenFieldAbility(3, 100, 80, 11));
