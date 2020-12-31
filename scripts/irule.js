c2f7bc6be30f09b89e9ac104bf94724d2972e1e7
theangelurd <71867069+theangelurd@users.noreply.github.com>
2020-12-30 20:43:01 -0400

    Create irule.js

---
+++ scripts/irule.js
@@ -0,0 +1,5 @@
+const irule = extendContent(mech, "irule", {});
+irule.constructor = function(){
+  return extend(PayloadUnit, {});
+};
+unitName.abilities.add(new ForceFieldAbility(16, 8/60, 280, 80 * 5, new ShieldRegenFieldAbility(3, 100, 80, 11));
