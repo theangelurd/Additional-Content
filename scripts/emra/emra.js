f4b4314d10efb376eebaaa4604bc3553a39a0807
theangelurd <71867069+theangelurd@users.noreply.github.com>
2021-01-01 14:57:09 -0400

    Update emra.js

--- scripts/emra/emra.js
+++ scripts/emra/emra.js
@@ -1,16 +1,24 @@
-const templura = new Planet("emra", Planets.sun, 1.2, 0.9);
+const emra = new Planet("emra", Planets.sun, 4, 0.8);
 emra.generator = new SerpuloPlanetGenerator();
-emra.mesh = new HexMesh(emra, 6);
-emra.bloom = true;
-emra.radius = 2;
+emra.mesh = new HexMesh(emra, 5);
+emra.bloom = false;
+emra.radius = 1;
 emra.accessible = true;
 emra.hasAtmosphere = true;
-emra.atmosphereColor = Color.valueOf("c7fffbff");
-emra.atmosphereRadIn = 0.02;
-emra.atmosphereRadOut = 0.3;
+emra.atmosphereColor = Color.valueOf("7d95d6");
+emra.atmosphereRadIn = 0.001;
+emra.atmosphereRadOut = 0.05;
 emra.localizedName = "Emra";
 
-const navalValley = new SectorPreset("navalValley", emra, 18);
-navalValley.captureWave = 25;
-navalValley.localizedName = "Naval Valley";
-navalValley.difficulty = 4;
+  const navalValley = new SectorPreset("navalValley", emra, 18);
+  navalValley.captureWave = 25;
+  navalValley.localizedName = "Naval Valley";
+  navalValley.difficulty = 4;
+  navalValley.alwaysUnlocked = false;
+  navalValley.research = SectorPresets.ruinousShores;
+
+  const planetEntryPoint = new SectorPreset("planetEntryPoint", emra, 21);
+  navalValley.captureWave = 15;
+  navalValley.localizedName = "Planet Entry Point";
+  navalValley.difficulty = 2;
+  navalValley.alwaysUnlocked = true;
