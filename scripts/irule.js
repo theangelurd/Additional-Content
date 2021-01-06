3b36e13225590b57d631951a1cfaa9302804d81b
theangelurd <71867069+theangelurd@users.noreply.github.com>
2021-01-05 21:15:21 -0400

    Update irule.js

--- scripts/irule.js
+++ scripts/irule.js
@@ -1,2 +1,3 @@
 const irule = extendContent(UnitType, "irule", {});
-irule.abilities.add(new ForceFieldAbility(12, 0.13, 260, 80 * 5), new ShieldRegenFieldAbility(8, 120, 80, 10));
+irule.abilities.add(new ForceFieldAbility(12, 0.13, 260, 80 * 5));
+irule.abilities.add(new ShieldRegenFieldAbility(8, 120, 80, 10));
