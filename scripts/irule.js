5d3cb52989b65e6760340e71d140fb48fbc6fdb2
theangelurd <71867069+theangelurd@users.noreply.github.com>
2021-01-05 21:25:38 -0400

    Update irule.js

--- scripts/irule.js
+++ scripts/irule.js
@@ -1,5 +1,5 @@
 const BlueGroundT2 = extendContent(UnitType, "protidae", {});
-BlueGroundT2.constructor = () => extend(UnitWaterMove, {});
+BlueGroundT2.constructor = () => extend(MechUnit, {});
 BlueGroundT2.abilities.add(new ForceFieldAbility(12, 0.13, 260, 80 * 5));
 BlueGroundT2.abilities.add(new ShieldRegenFieldAbility(8, 120, 80, 10));
 BlueGroundT2.ammoType = AmmoTypes.power;
