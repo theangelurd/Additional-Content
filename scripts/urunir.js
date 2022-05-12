cd57f9af8389c3a09899cc2910576f470f2892ec
Tayros <71867069+theangelurd@users.noreply.github.com>
2022-05-12 13:48:28 -0400

    Create urunir.js

---
+++ scripts/urunir.js
@@ -0,0 +1,26 @@
+const generators = require("extras/urunirGen");
+const urunir = extend(Planet, "urunir", Planets.sun, 2, 2, {
+    generator: Version.number >= 7 ? Planets.serpulo.generator : generators.urunir,
+    meshLoader: () => new HexMesh(this, 7),
+    bloom: true,
+    radius: 3,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("db8d6e"),
+    atmosphereRadIn: 0.1,
+    atmosphereRadOut: 0.15,
+    localizedName: "Urunir"
+});
+templura.meshLoader = () => extend(HexMesh, templura, 7, {});
+
+const landingPoint = extend(SectorPreset, "landing-point", urunir, 0, {
+    description: "The initial starting point for the harsh planet Urunir.",
+    localizedName: "Landing Point",
+    difficulty: 3,
+    alwaysUnlocked: true
+});
+
+module.exports = {
+    urunir: urunir,
+    landingPoint: landingPoint,
+};
